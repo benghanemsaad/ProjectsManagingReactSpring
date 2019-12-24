f931a489dc157f3c94d7371829146ac8021fe159
benmerieme <ismail.benmerieme@uit.ac.ma>
2019-12-24 20:01:15 +0100

    commit after admin push

--- spring-social/src/main/java/com/example/springsocial/controller/UserController.java
+++ spring-social/src/main/java/com/example/springsocial/controller/UserController.java
@@ -2,7 +2,9 @@ package com.example.springsocial.controller;
 
 import com.example.springsocial.exception.ResourceNotFoundException;
 import com.example.springsocial.model.User;
+import com.example.springsocial.repository.ProjetRepository;
 import com.example.springsocial.repository.UserRepository;
+import com.example.springsocial.repository.ValidateProjectEmpRepository;
 import com.example.springsocial.security.CurrentUser;
 import com.example.springsocial.security.UserPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,12 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private ProjetRepository projetRepository;
+
+    @Autowired
+    private ValidateProjectEmpRepository validateProjectEmpRepository;
 
     @GetMapping("/user/me")
     @PreAuthorize("hasRole('USER')")
